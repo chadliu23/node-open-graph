4a2c2bc7793751d5ad281ea45aac500e3992d949
Chad Liu <liu_q@worksap.co.jp>
2015-06-02 17:46:56 +0800

    Remove uncaughtException in the lib

--- index.js
+++ index.js
@@ -211,9 +211,3 @@ function extractMeta(ptr, keys, content){
 		ptr[key] = [ ptr[key], content ];
 	}
 }
-
-process.on('uncaughtException', function (err) {
-  console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
-  console.error(err.stack)
-  process.exit(1)
-})
